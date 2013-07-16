181090ca405428ebb7b2444eb863e7fddf3ec01f
Peter Darrow <peter@sheepdoginc.ca>
2013-07-16 14:44:50 -0300

    Separate project setup from template setup.

--- new_project.sh
+++ new_project.sh
@@ -13,28 +13,21 @@ function echo_exit {
     exit 1
 }
 
-## Check for required executables
+# Check for required executables
 for ex in python2.7 virtualenv virtualenvwrapper.sh
 do
     command -v $ex >/dev/null 2>&1 || { echo >&2 echo_exit "Executable '$ex' is required but not installed. Aborting."; }
 done
 
-# Install python dependencies
+# Create virtualenv with just Django
 source `which virtualenvwrapper.sh`
 mkvirtualenv --python=python2.7 --no-site-packages $PROJECT
-pip install -r requirements.txt
+pip install https://www.djangoproject.com/download/1.6b1/tarball/
 
 # Create project based on django-base template
 django-admin.py startproject --template=https://github.com/SheepDogInc/django-base/archive/project-template.zip --extension=py,json,md,sh,bowerrc --name=Procfile $PROJECT
 rm new_project.sh
 
-# Install remaining dependencies
-npm install
-printf 'export PATH=%s/node_modules/.bin:$PATH\n' `pwd` >> $VIRTUAL_ENV/bin/postactivate
-workon $PROJECT
-bower install
-
 echo "============================"
 echo "TO PROCEED:"
-echo "(1) => Create database $PROJECT"
-echo "(2) ./manage.py syncdb"
+echo "(1) ./setup.sh"
